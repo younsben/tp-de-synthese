3987f06e0c5aa0fa6d2b959110cc65cef5a8613b
Leo-Tinkeam <sarcyleo@gmail.com>
2024-12-09 11:22:20 +0100

    Add files via upload

--- tp1_q1.c
+++ tp1_q1.c
@@ -4,18 +4,23 @@
 #include <sys/wait.h>
 #include <string.h>
 
-#define nbCharExit 4
 #define msgHello "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
+#define enseash "enseash % "
 #define strExit "exit"
+#define nbCharExit 4
 #define wCon 1
 #define rCon 0
+#define readSize 128
 
 int main()
 {
     write(wCon, msgHello, sizeof(msgHello)); //Envoie msgHello dans la console
-    char* entree;
-    while (strncmp(entree, strExit, nbCharExit)) { //Attends que l'utilisateur tape "exit"
-        read(rCon, entree, sizeof(strExit));
+    char entree[readSize];
+    int inputSize;
+    while (strcmp(entree, strExit)) { //Attends que l'utilisateur tape "exit"
+        write(wCon, enseash, sizeof(enseash));
+        inputSize = read(rCon, entree, readSize);
+        entree[inputSize-1] = '\0'; //On met le délimiteur de fin de chaine
     }
     exit(EXIT_SUCCESS);
 }
