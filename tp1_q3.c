3c22aa91e0d82dff6b9f5f611f8a367299e80f5d
Leo-Tinkeam <sarcyleo@gmail.com>
2024-12-09 11:23:12 +0100

    Add files via upload

--- tp1_q3.c
+++ tp1_q3.c
@@ -6,7 +6,8 @@
 
 #define msgHello "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
 #define enseash "enseash % "
-#define msgBye "Bye bye ..."
+#define msgBye "Bye bye..."
+#define msgCommande "Cette commande n'existe pas ou prends des arguments\n"
 #define strExit "exit"
 #define nbCharExit 4
 #define strFortune "fortune"
@@ -14,24 +15,32 @@
 #define wCon 1
 #define rCon 0
 #define readSize 128
+#define bufferSize 64
 
 int main()
 {
     write(wCon, msgHello, sizeof(msgHello)); //Envoie msgHello dans la console
-    char* entree;
+    char entree[readSize];
+    int status;
+    char buffer[bufferSize];
+    int inputSize;
     while (1) {
         write(wCon, enseash, sizeof(enseash));
-        read(rCon, entree, readSize);
-        if (strncmp(entree, strExit, nbCharExit) == 0) { //Si l'utilisateur tape exit, on break
+        inputSize = read(rCon, entree, readSize);
+        entree[inputSize-1] = '\0'; //On met le délimiteur de fin de chaine
+
+        if (strcmp(entree, strExit) == 0) { //Si l'utilisateur tape exit, on break
             write(wCon, msgBye, sizeof(msgBye));
-            break;
-        } else if (strncmp(entree, strFortune, nbCharFortune) == 0) { //S'il tape fortune, on execute fortune dans un fils
-            int status;
-            if (fork() == 0) {
-                execlp("fortune", "fortune", (char*) NULL);
-            }
-            wait(&status);
+            exit(EXIT_SUCCESS);
         }
+
+        if (fork() == 0) { //On exécute la commande dans un fils
+            if (execvp(entree, (char*) NULL) == -1) {
+                write(wCon, msgCommande, sizeof(msgCommande));
+                exit(EXIT_FAILURE);
+            }
+        }
+        wait(&status);
     }
     exit(EXIT_SUCCESS);
 }
