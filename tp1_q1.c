921ad19a9476af51e15faf0278c6e818bac6cc5b
Leo-Tinkeam <sarcyleo@gmail.com>
2024-12-09 09:00:24 +0100

    Add files via upload

--- tp1_q1.c
+++ tp1_q1.c
@@ -6,13 +6,13 @@
 
 #define nbCharExit 4
 #define msgHello "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
+#define strExit "exit"
 
 int main()
 {
     write(1, msgHello, sizeof(msgHello));
     char* entree;
-    char* strExit = "exit";
-    while (strncmp(entree, strExit, nbCharExit)) {
+    while (strncmp(entree, strExit, nbCharExit)) { //Attends que l'utilisateur tape "exit"
         read(0, entree, sizeof(strExit));
     }
     exit(EXIT_SUCCESS);
