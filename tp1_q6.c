f382717ae726e72aea8e58af941d3ebc6534d8fb
Leo-Tinkeam <sarcyleo@gmail.com>
2024-12-16 11:39:48 +0100

    Add files via upload

--- tp1_q6.c
+++ tp1_q6.c
@@ -10,11 +10,9 @@
 #define enseashExit "enseash [exit:%d|%dms] "
 #define enseashSign "enseash [sign:%d|%dms] "
 #define msgBye "Bye bye..."
-#define msgCommande "Cette commande n'existe pas ou prends des arguments\n"
+#define msgCommand "Cette commande n'existe pas ou prends des arguments\n"
 #define strExit "exit"
 #define nbCharExit 4
-#define strFortune "fortune"
-#define nbCharFortune 7
 #define wCon 1
 #define rCon 0
 #define readSize 128
@@ -23,10 +21,12 @@
 #define nbArgs 16
 #define lengthArgs 16
 
-int main()
-{
-    write(wCon, msgHello, sizeof(msgHello)); //Envoie msgHello dans la console
-    char entree[readSize];
+void exitBye(char* input, int inputSize);
+void decodeAndExecuteCommand(int* statusAdd, char* input, struct timespec* ts1Add, char** args);
+
+int main() {
+    write(wCon, msgHello, sizeof(msgHello)); //Send msgHello to the console
+    char input[readSize];
     char** args = malloc(nbArgs*sizeof(char*));
     int status = -2;
     char buffer[bufferSize];
@@ -45,7 +45,7 @@ int main()
                 sprintf(buffer, enseashSign, WTERMSIG(status), (ts2.tv_nsec-ts1.tv_nsec)/1000000);
             }
             strcat(buffer, "%");
-            for (int i = 0; i < bufferSize; i++) { //On retire les caractères inutiles après le '%'
+            for (int i = 0; i < bufferSize; i++) { //Remove useless caractère after "%"
                 if (buffer[i] == percentChar) {
                     for (int j = i+1; j < bufferSize; j++) {
                         buffer[j] = '\0';
@@ -55,42 +55,49 @@ int main()
             }
             write(wCon, buffer, sizeof(buffer));
         }
-        inputSize = read(rCon, entree, readSize);
-        entree[inputSize-1] = '\0'; //On met le délimiteur de fin de chaine
+        inputSize = read(rCon, input, readSize);
+        input[inputSize-1] = '\0'; //Add the delimiter to the end of the string
 
-        clock_gettime(CLOCK_REALTIME, &ts1);
-        if (strcmp(entree, strExit) == 0 || inputSize == 0) { //Si l'utilisateur tape exit ou si il a fait Ctrl+D, on exit
-            write(wCon, msgBye, sizeof(msgBye));
-            exit(EXIT_SUCCESS);
-        }
+        exitBye(input, inputSize);
 
-        int j = 0;
-        int n = 0;
-        args[0] = malloc(lengthArgs*sizeof(char));
-        for (int i = 0; i < readSize; i++) { //Séparation des arguments
-            if (entree[i] == '\0') {
-                args[n][j] = '\0';
-                args[n+1] = NULL;
-                break;
-            }
-            if (entree[i] == ' ') {
-                args[n][j] = '\0';
-                args[n+1] = malloc(lengthArgs*sizeof(char));
-                n++;
-                j = 0;
-            } else {
-                args[n][j] = entree[i];
-                j++;
-            }
-        }
-
-        if (fork() == 0) { //On exécute la commande dans un fils
-            if (execvp(args[0], args) == -1) {
-                write(wCon, msgCommande, sizeof(msgCommande));
-                exit(EXIT_FAILURE);
-            }
-        }
-        wait(&status);
+        decodeAndExecuteCommand(&status, input, &ts1, args);
     }
     exit(EXIT_SUCCESS);
+}
+
+void exitBye(char* input, int inputSize) {
+    if (strcmp(input, strExit) == 0 || inputSize == 0) { //If the user typed "exit" or press Ctrl+D, close the shell
+        write(wCon, msgBye, sizeof(msgBye));
+        exit(EXIT_SUCCESS);
+    }
+}
+
+void decodeAndExecuteCommand(int* statusAdd, char* input, struct timespec* ts1Add, char** args) {
+    int j = 0;
+    int n = 0;
+    args[0] = malloc(lengthArgs*sizeof(char));
+    for (int i = 0; i < readSize; i++) { //Split the arguments
+        if (input[i] == '\0') {
+            args[n][j] = '\0';
+            args[n+1] = NULL;
+            break;
+        }
+        if (input[i] == ' ') {
+            args[n][j] = '\0';
+            args[n+1] = malloc(lengthArgs*sizeof(char));
+            n++;
+            j = 0;
+        } else {
+            args[n][j] = input[i];
+            j++;
+        }
+    }
+    clock_gettime(CLOCK_REALTIME, ts1Add);
+    if (fork() == 0) { //Execute the command in a child process
+        if (execvp(args[0], args) == -1) {
+            write(wCon, msgCommand, sizeof(msgCommand));
+            exit(EXIT_FAILURE);
+        }
+    }
+    wait(statusAdd);
 }
